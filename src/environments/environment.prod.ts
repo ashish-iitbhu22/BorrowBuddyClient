4629d3ade7553b83bac1ecc9c360aed3327acc4a
Ashish Kumar <ashish.iitbhu22@gmail.com>
2025-01-15 11:31:56 +0530

    firebase config in env prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,14 @@
 export const environment = {
   production: true,
   API_HOST: 'https://borrowbuddyserver.onrender.com',
+  firebaseConfig: {
+    apiKey: 'AIzaSyBVDgT_ya_kxLxk0jpX3KUWDGRiBSpH7Jk',
+    authDomain: 'borrowbuddyclient.firebaseapp.com',
+    projectId: 'borrowbuddyclient',
+    storageBucket: 'borrowbuddyclient.firebasestorage.app',
+    messagingSenderId: '603287497008',
+    appId: '1:603287497008:web:28cde7b38fd28703a780f5',
+    measurementId: 'G-C3J4XLVE7R',
+  },
 };
 
